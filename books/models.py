5641c15d09c10a2296d6f474fee8c0ac9870f4b9
Maxime Perreault <maxime.perreault@gmail.com>
2016-10-18 23:43:28 +0000

    add email field to author model

--- books/models.py
+++ books/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    email = models.EmailField(blank=True, null=True)
 
     def __unicode__(self):
         return self.name
