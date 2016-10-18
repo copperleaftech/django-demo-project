5cb34b5030de94fb8dc15d12f1eb82841ce26dd4
Maxime Perreault <maxime.perreault@gmail.com>
2016-10-18 22:43:31 +0000

    Add factories to generate books, categories, and authors. To be used in tests

---
+++ books/factories.py
@@ -0,0 +1,38 @@
+import factory
+
+from .models import Category, Book, Author
+
+from faker import Faker
+fake = Faker()
+
+
+class AuthorFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Author
+
+    name = factory.Faker('name')
+
+
+class CategoryFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Category
+
+    name = factory.Faker('word')
+
+
+class BookFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Book
+        django_get_or_create = ('isbn',)
+
+    name = factory.Faker('sentence')
+    isbn = factory.Faker('ean13')
+    author = factory.SubFactory(AuthorFactory)
+
+    @factory.post_generation
+    def categories(self, create, count=5, **kwargs):
+        make_category = getattr(CategoryFactory, 'create' if create else 'build')
+
+        categories = [make_category() for i in range(count)]
+        for category in categories:
+            self.categories.add(category)
