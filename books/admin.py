5630a75af9eaef5c77af31be1dad7af4ad402beb
Maxime Perreault <maxime.perreault@gmail.com>
2016-10-18 21:54:38 +0000

    add default admins for all models in the book app

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Book, Author
+
+
+admin.site.register(Category)
+admin.site.register(Book)
+admin.site.register(Author)
