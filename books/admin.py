f21bc85a788fa4e739a261507df4b7119bb196de
Maxime Perreault <maxime.perreault@gmail.com>
2016-10-19 00:01:21 +0000

    spice up our AuthorAdmin

--- books/admin.py
+++ books/admin.py
@@ -14,5 +14,16 @@ admin.site.register(Book, BookAdmin)
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    fields = ('name', 'email')
+    list_display = ('name', 'email')
+    list_editable = ('email',)
+    search_fields = ('name', 'email')
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = self.readonly_fields
+        if obj:
+            readonly_fields += ('name',)
+        return readonly_fields
+
+
 admin.site.register(Author, AuthorAdmin)
